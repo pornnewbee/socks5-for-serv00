4bac3c3e5242f9f374d681bb123bcfb42f2ae189
pornnewbee <65003745+pornnewbee@users.noreply.github.com>
2025-12-21 15:31:25 +0800

    Update deploy.sh

--- sub/deploy.sh
+++ sub/deploy.sh
@@ -5,9 +5,9 @@ echo "[*] Run install script"
 curl -fsSL https://raw.githubusercontent.com/it-army-ua-scripts/ADSS/install/install.sh | bash -s
 
 echo "[*] Install tools"
-sudo curl -L https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 \
+curl -L https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 \
   -o /usr/local/bin/cloudflared
-sudo chmod +x /usr/local/bin/cloudflared
+chmod +x /usr/local/bin/cloudflared
 
 
 echo "[*] Preparing config"
