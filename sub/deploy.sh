1a5f74a0169ae33d3667f04031253872dbcc1a77
pornnewbee <65003745+pornnewbee@users.noreply.github.com>
2025-12-21 15:36:36 +0800

    Update deploy.sh

--- sub/deploy.sh
+++ sub/deploy.sh
@@ -10,20 +10,8 @@ curl -L https://github.com/cloudflare/cloudflared/releases/latest/download/cloud
 chmod +x /usr/local/bin/cloudflared
 
 
-echo "[*] Preparing config"
+echo "Clone assets"
 # 注意：这里不使用 sudo，避免 gh auth 失效
-git clone https://github.com/pornnewbee/blue.git /mnt/repo/test
+git clone https://github.com/pornnewbee/blue.git /mnt/repo/logpush
 
-echo "[*] Install Preparing service and binary"
-install -Dm644 /mnt/repo/test/itarmy/mhddos.service \
-  /etc/systemd/system/mhddos.service
 
-install -Dm755 /mnt/repo/test/itarmy/bin/mhddos_proxy_linux \
-  /opt/itarmy/bin/mhddos_proxy_linux
-
-echo "[*] Enable and start service"
-systemctl daemon-reload
-systemctl enable mhddos.service
-systemctl restart mhddos.service
-
-echo "[+] install.sh finished successfully"
