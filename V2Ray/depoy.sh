af7231dbe069ee9d102283e8d92a82f0dba7d70a
pornnewbee <65003745+pornnewbee@users.noreply.github.com>
2025-12-21 22:43:49 +0800

    Update depoy.sh

--- V2Ray/depoy.sh
+++ V2Ray/depoy.sh
@@ -2,3 +2,5 @@ curl -L -o install-v2ray.sh https://raw.githubusercontent.com/v2fly/fhs-install-
 sudo bash install-v2ray.sh
 curl -L -o install-dat-release.sh https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh
 sudo bash install-dat-release.sh
+
+sudo nano /usr/local/etc/v2ray/config.json
