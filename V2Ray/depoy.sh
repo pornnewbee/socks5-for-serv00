422cf332fc0822141845d8ad07074e82aae8134f
pornnewbee <65003745+pornnewbee@users.noreply.github.com>
2025-12-21 13:34:50 +0000

    a

---
+++ V2Ray/depoy.sh
@@ -0,0 +1,4 @@
+curl -L -o install-v2ray.sh https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
+sudo bash install-v2ray.sh
+curl -L -o install-dat-release.sh https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh
+sudo bash install-dat-release.sh
