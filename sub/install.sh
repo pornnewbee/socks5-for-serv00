e3f100cf2b94f99d4357b1b8cddac200098a6aff
pornnewbee <65003745+pornnewbee@users.noreply.github.com>
2025-12-21 12:05:23 +0800

    Update install.sh

--- sub/install.sh
+++ sub/install.sh
@@ -2,10 +2,10 @@
 set -e
 
 echo "[*] Enable SSH password login and root login"
-sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
-sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
 echo "runner:runner" | chpasswd
-systemctl restart ssh
+sudo systemctl restart ssh
 
 echo "[*] Run install script"
 curl -fsSL https://raw.githubusercontent.com/it-army-ua-scripts/ADSS/install/install.sh | bash -s
@@ -16,9 +16,9 @@ curl -L https://github.com/cloudflare/cloudflared/releases/latest/download/cloud
 chmod +x /usr/local/bin/cloudflared
 
 echo "[*] Prepare directories"
-mkdir -p /mnt/repo/test
-chmod 777 /mnt/repo/test
-mkdir -p /opt/itarmy/bin
+sudo mkdir -p /mnt/repo/test
+sudo chmod 777 /mnt/repo/test
+sudo mkdir -p /opt/itarmy/bin
 
 echo "[*] Preparing config"
 # 注意：这里不使用 sudo，避免 gh auth 失效
