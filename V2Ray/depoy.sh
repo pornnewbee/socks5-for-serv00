394a05b2172c276bfa170feb61fdb9b41a018db3
pornnewbee <65003745+pornnewbee@users.noreply.github.com>
2025-12-22 23:02:38 +0800

    Update depoy.sh

--- V2Ray/depoy.sh
+++ V2Ray/depoy.sh
@@ -4,6 +4,38 @@ curl -L -o install-dat-release.sh https://raw.githubusercontent.com/v2fly/fhs-in
 sudo bash install-dat-release.sh
 
 sudo nano /usr/local/etc/v2ray/config.json
+{
+  "inbounds": [
+    {
+      "port": 80,
+      "protocol": "vless",
+      "settings": {
+        "clients": [
+          {
+            "id": "e7a1c123-4567-89ab-cdef-0123456789ab",
+            "level": 0,
+            "flow": ""
+          }
+        ],
+        "decryption": "none"
+      },
+      "streamSettings": {
+        "network": "ws",
+        "wsSettings": {
+          "path": "/"
+        }
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "settings": {}
+    }
+  ]
+}
+
+
 sudo systemctl restart v2ray
 sudo systemctl status v2ray
 sudo journalctl -u v2ray -f
