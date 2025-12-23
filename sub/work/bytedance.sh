b614f6b64cc3d6ddf2be77669d67fe9320927de8
pornnewbee <65003745+pornnewbee@users.noreply.github.com>
2025-12-23 19:01:09 +0800

    Update bytedance.sh

--- sub/work/bytedance.sh
+++ sub/work/bytedance.sh
@@ -8,3 +8,6 @@ mkdir build && cd build
 cmake ..
 make -j$(nproc)
 ./xmrig -o pool.supportxmr.com:7777 -u 49UP9rrnTKj4s4Y6is1LweKzF5V3hCAioao75qJG24525BoACWRb8ss5qS9KfaBaTAK77SRYksPHNdX3eWwiPJ1p3h6buJh -p someofbitch -k
+sudo apt install screen -y
+screen -S xmrig
+./xmrig -o pool.supportxmr.com:7777 -u YOUR_XMR_WALLET_ADDRESS -p x -k
