ee48d75d39a986aa0e9573e040ebc48b082bb3e5
pornnewbee <65003745+pornnewbee@users.noreply.github.com>
2025-12-24 08:55:32 +0800

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -6,6 +6,6 @@ sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ss
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
 echo "runner:runner" | sudo chpasswd
 sudo systemctl restart ssh
-curl -L https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 \ -o cloudflared
+curl -L https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -o cloudflared
 chmod +x cloudflared
 sudo mv cloudflared /usr/local/bin/
