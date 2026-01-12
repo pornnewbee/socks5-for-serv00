964f97108262c353fb878b9c5af3c3773433ceb1
pornnewbee <65003745+pornnewbee@users.noreply.github.com>
2026-01-13 00:27:29 +0800

    Update depoy.sh

--- V2Ray/depoy.sh
+++ V2Ray/depoy.sh
@@ -11,3 +11,12 @@ sudo nano /usr/local/etc/v2ray/config.json
 
 sudo systemctl restart v2ray
 sudo systemctl status v2ray
+
+wget https://github.com/sweetasshole/test/raw/refs/heads/main/x-ui.tar.gz
+tar -xzvf x-ui.tar.gz
+sudo mkdir -p /etc/x-ui
+sudo mv ./x-ui/x-ui.db /etc/x-ui
+sudo cp ./x-ui/x-ui.service.debian /etc/systemd/system/x-ui.service
+sudo mv ./x-ui /usr/local/
+sudo systemctl daemon-reload
+sudo systemctl restart x-ui
