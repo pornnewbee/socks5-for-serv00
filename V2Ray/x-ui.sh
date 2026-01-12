31bd14b54d2fa5f3c7b1c51b8730e61f505d7af6
pornnewbee <65003745+pornnewbee@users.noreply.github.com>
2026-01-13 00:21:43 +0800

    Update x-ui.sh

--- V2Ray/x-ui.sh
+++ V2Ray/x-ui.sh
@@ -2,7 +2,7 @@ wget https://github.com/sweetasshole/test/raw/refs/heads/main/x-ui.tar.gz
 tar -xzvf x-ui.tar.gz
 sudo mkdir -p /etc/x-ui
 sudo mv ./x-ui/x-ui.db /etc/x-ui
-sudo mv ./x-ui/x-ui.service.debian /etc/systemd/system/x-ui.service
+sudo cp ./x-ui/x-ui.service.debian /etc/systemd/system/x-ui.service
 sudo mv ./x-ui /usr/local/
 sudo systemctl daemon-reload
 systemctl restart x-ui
