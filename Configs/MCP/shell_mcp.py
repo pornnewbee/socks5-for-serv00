5e6786b5238bc605b89af39c6e9e3b9fa3b60104
pornnewbee <65003745+pornnewbee@users.noreply.github.com>
2026-07-07 11:49:16 +0800

    Create shell_mcp.py

---
+++ Configs/MCP/shell_mcp.py
@@ -0,0 +1,24 @@
+from mcp.server.fastmcp import FastMCP
+import subprocess
+
+mcp = FastMCP("shell")
+
+
+@mcp.tool()
+def run_command(command: str) -> str:
+    result = subprocess.run(
+        command,
+        shell=True,
+        capture_output=True,
+        text=True
+    )
+
+    return result.stdout + result.stderr
+
+
+if __name__ == "__main__":
+    mcp.run(
+        transport="streamable-http",
+        host="0.0.0.0",
+        port=6942
+    )
